73120a9b46cdc9ad635f7357fc43301cce6bbc5b
Andreea <andreea.elena.circiumaru@gmail.com>
2013-11-20 00:33:55 +0200

    Commit 1

---
+++ Ziua 2/tabel.php
@@ -0,0 +1,74 @@
+<?DOCTYPE html>
+<html>
+	<head>
+		<title>Tabel angajati</title>
+		<link rel="styesheet" href="stil.css" />
+	</head>
+
+	<body>
+		<h2>Tabel</h2>
+
+		<table class="main-table">
+			<thead>
+				<tr>
+					<td>Nume</td>
+					<td>Prenume</td>
+					<td>Varsta</td>
+					<td>Departament</td>
+					<td>Data angajarii</td>
+					<td>Pozitia ocupata</td>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr>
+					<td>Nume1</td>
+					<td>Prenume1</td>
+					<td>10</td>
+					<td>Departament1</td>
+					<td>1.10.2010</td>
+					<td>Pozitia1</td>
+				</tr>
+
+				<tr>
+					<td>Nume2</td>
+					<td>Prenume2</td>
+					<td>20</td>
+					<td>Departament2</td>
+					<td>2.10.2010</td>
+					<td>Pozitia2</td>
+				</tr>
+
+				<tr>
+					<td>Nume3</td>
+					<td>Prenume3</td>
+					<td>30</td>
+					<td>Departament3</td>
+					<td>3.10.2010</td>
+					<td>Pozitia3</td>
+				</tr>
+
+				<tr>
+					<td>Nume4</td>
+					<td>Prenume4</td>
+					<td>40</td>
+					<td>Departament4</td>
+					<td>4.10.2010</td>
+					<td>Pozitia4</td>
+				</tr>
+
+				<tr>
+					<td>Nume5</td>
+					<td>Prenume5</td>
+					<td>50</td>
+					<td>Departament5</td>
+					<td>5.10.2010</td>
+					<td>Pozitia5</td>
+				</tr>
+
+				<tr>
+				</tr>
+			</tbody>
+
+	</body>
+</html>
